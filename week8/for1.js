c9e34145da2c262605576c030a7292b4a949c5d9
yucing <e97456797979@gmail.com>
2021-06-12 19:58:02 +0800

    add

---
+++ week8/for1.js
@@ -0,0 +1,11 @@
+var a=[1,2,3,4,5]
+
+for(var i of a)
+{
+    console.log("i=",i)
+}
+
+for(var i of a)
+{
+    console.log("i=%s",i)
+}
---
+++ week8/for2.js
@@ -0,0 +1,6 @@
+var a=[2,4,6,8]
+
+for(var i in a)
+{
+    console.log("a[%s]=%d",i,a[i])
+}
